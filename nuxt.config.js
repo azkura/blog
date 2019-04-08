2ecfa13364bc2bd97a4c59080c9d2c4c46ae2148
az <azizkoura@hotmail.com>
2019-04-08 10:11:56 +0200

    generate module config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 import pkg from './package'
+const axios = require("axios")
 
 export default {
   mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
+  
+  // Headers of the page
   head: {
     title: pkg.description,
     meta: [
@@ -19,39 +19,47 @@ export default {
     ]
   },
 
-  /*
-  ** Customize the progress-bar color
-  */
+  //Customize the progress-bar color
   loading: { color: '#fff' },
 
-  /*
-  ** Global CSS
-  */
+  
+  //Global CSS
   css: [
   ],
 
-  /*
-  ** Plugins to load before mounting the App
-  */
+  // Plugins to load before mounting the App
   plugins: [
   ],
 
-  /*
-  ** Nuxt.js modules
-  */
+  // Nuxt.js modules
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    
+    '@nuxtjs/axios',
+
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
 
-    // storyblok config
+    // CMS storyblok modudes
     [ 
       "storyblok-nuxt", 
       { accessToken: process.env.NODE_ENV == "production" ? "Q8XoHn2JU5Wyr49Xdm7jlQtt" :
       " mrTgqb8PhauQr1YGRFaJJgtt", cacheProvider:"memory"}
     ]
-   
   ],
+  generate:{
+    routes(){
+      return axios.get("https://api.storyblok.com/v1/cdn/stories?version=published&token=Q8XoHn2JU5Wyr49Xdm7jlQtt&starts_with=blog&cv="
+      + Math.floor(Date.now() / 1e3)
+      ).then(res => {
+        const blogPosts = res.data.stories.map(bp => bp.full_slug)
+        return [
+          '/',
+          '/blog',
+          '/about',
+          ...blogPosts
+        ]
+      })
+    }
+  },
   /*
   ** Axios module configuration
   */
